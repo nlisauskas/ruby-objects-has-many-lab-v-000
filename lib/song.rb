6ab863faa43230339697caa483454961914128be
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-20 02:17:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,10 +7,7 @@ attr_accessor :name, :artist
   end
 
   def artist_name
-    if Artist.songs.include?(self)
-      return Artist.name
-    else return nil
-    end
+    artist ? artist.name : nil
   end
 
 end
